6487c050ac1143e426d9d2b4ecb2d3b1655bc5db
nandini252005 <nandini.midde25@gmail.com>
2026-09-11 21:52:05 +0530

    Fix erros

--- server/src/config/gemini.config.js
+++ server/src/config/gemini.config.js
@@ -2,7 +2,7 @@ import { GoogleGenAI } from "@google/genai";
 const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
-const MODEL_NAME = "gemini-2.5-flash";
+const MODEL_NAME = "gemini-flash-latest";
 const generateContent = async (prompt) => {
   try {
     const response = await ai.models.generateContent({
